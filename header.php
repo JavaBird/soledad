c37f7b1075ff4c08ea20bfbc342a91ecf5762afe
Curious <baidu211@vip.qq.com>
2016-03-22 23:16:12 +0800

    modify
    modify

---
+++ header.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Soledad</title>
+    <!--[if lt IE 9]>
+    <script src="<?php get_template_directory_uri()?>js/html5.js"></script>
+    <style type="text/css">
+        .featured-carousel .item {
+            opacity: 1;
+        }
+    </style>
+    <![endif]-->
+
+    <?php wp_head() ?>
+</head>
+<body class="home blog penci-body-boxed">
+<a id="close-sidebar-nav" class="header-3"><i class="fa fa-close"></i></a>
+
+<nav id="sidebar-nav" class="header-3">
+
+<div id="sidebar-nav-logo">
+    <a href=""><img
+            src="http://pencidesign.com/soledad/soledad-magazine/wp-content/uploads/sites/7/2015/09/logo-nav.png"
+            alt="Soledad Magazine"/></a>
+</div>
+
+<?php wp_nav_menu();?>
+
+</nav>
+
+<!-- .wrapper-boxed -->
+<div class="wrapper-boxed header-style-header-3 enable-boxed">
+
+<!-- Top Bar -->
+<div class="penci-top-bar">
+    <div class="container">
+        <div class="penci-headline">
+            <span class="headline-title">Top Posts</span>
+
+            <div class="penci-topbar-social">
+                <div class="inner-header-social">
+                    <a href="https://www.facebook.com/PenciDesign" target="_blank"><i class="fa fa-facebook"></i></a>
+                    <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+                    <a href="#" target="_blank"><i class="fa fa-google-plus"></i></a>
+                    <a href="#" target="_blank"><i class="fa fa-instagram"></i></a>
+                    <a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
+                    <a href="#" target="_blank"><i class="fa fa-youtube-play"></i></a>
+                    <a href="#" target="_blank"><i class="fa fa-rss"></i></a>
+                </div>
+            </div>
+            <div class="penci-headline-posts" data-auto="true" data-autotime="3000" data-speed="300"
+                 data-direction="vertical">
+                <div>
+                    <a class="penci-topbar-post-title"
+                       href="http://pencidesign.com/soledad/soledad-magazine/apple-haptic-tech-is-a-peak-at-the-ui-of-the-future/">Apple’s
+                        Haptic Tech Is a peak at the...</a>
+                </div>
+                <div>
+                    <a class="penci-topbar-post-title"
+                       href="http://pencidesign.com/soledad/soledad-magazine/furniture-makes-this-office-look-like-a-toy-box/">Furniture
+                        Makes This Office Look Like a Toy...</a>
+                </div>
+                <div>
+                    <a class="penci-topbar-post-title"
+                       href="http://pencidesign.com/soledad/soledad-magazine/kermits-new-girlfriend-enrages-the-internet/">Kermit’s
+                        New Girlfriend Enrages the Internet</a>
+                </div>
+                <div>
+                    <a class="penci-topbar-post-title"
+                       href="http://pencidesign.com/soledad/soledad-magazine/what-its-like-to-travel-to-greece-right-now/">What
+                        It’s Like to Travel to Greece Right...</a>
+                </div>
+                <div>
+                    <a class="penci-topbar-post-title"
+                       href="http://pencidesign.com/soledad/soledad-magazine/canadian-rockies-rafting-near-banff-national-park/">Canadian
+                        Rockies Rafting near Banff National Park</a>
+                </div>
+                <div>
+                    <a class="penci-topbar-post-title"
+                       href="http://pencidesign.com/soledad/soledad-magazine/10-efficient-ways-to-save-time-in-daily-life/">10
+                        Efficient Ways to Save Time in Daily...</a>
+                </div>
+                <div>
+                    <a class="penci-topbar-post-title"
+                       href="http://pencidesign.com/soledad/soledad-magazine/apple-watch-vs-moto-360-which-one-should-you-buy-video/">Apple
+                        Watch vs Moto 360: Which one should...</a>
+                </div>
+                <div>
+                    <a class="penci-topbar-post-title"
+                       href="http://pencidesign.com/soledad/soledad-magazine/5-places-to-visit-in-your-lifetime/">5
+                        Places to visit in your Lifetime</a>
+                </div>
+                <div>
+                    <a class="penci-topbar-post-title"
+                       href="http://pencidesign.com/soledad/soledad-magazine/box-office-transporter-refueled-crashes-theater/">Box
+                        Office: &#8216;Transporter Refueled&#8217; Crashes Theater</a>
+                </div>
+                <div>
+                    <a class="penci-topbar-post-title"
+                       href="http://pencidesign.com/soledad/soledad-magazine/android-is-finally-getting-a-standalone-street-view-app/">Android
+                        is Finally Getting a Standalone Street View...</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<header id="header" class="header-header-3"><!-- #header -->
+<div class="inner-header">
+    <div class="container align-left-logo has-banner">
+
+        <div id="logo">
+            <h1>
+                <a href="http://pencidesign.com/soledad/soledad-magazine/"><img
+                        src="http://pencidesign.com/soledad/soledad-magazine/wp-content/uploads/sites/7/2015/09/logo.png"
+                        alt="Soledad Magazine"/></a>
+            </h1>
+        </div>
+
+        <div class="header-banner header-style-3">
+            <a href="#" target="_blank"><img
+                    src="http://pencidesign.com/soledad/soledad-magazine/wp-content/uploads/sites/7/2015/09/banner-header.jpg"
+                    alt="Banner"/></a></div>
+
+
+    </div>
+</div>
+
+<!-- Navigation -->
+<nav id="navigation" class="header-layout-bottom header-3">
+<div class="container">
+<div class="button-menu-mobile header-3"><i class="fa fa-bars"></i></div>
+
+    <?php wp_nav_menu();?>
+
+<div id="top-search">
+    <a class="search-click"><i class="fa fa-search"></i></a>
+
+    <div class="show-search">
+        <form role="search" method="get" id="searchform" action="http://pencidesign.com/soledad/soledad-magazine/">
+            <div>
+                <input type="text" class="search-input" placeholder="输入搜索内容" name="s" id="s"/>
+            </div>
+        </form>
+        <a class="search-click close-search"><i class="fa fa-close"></i></a>
+    </div>
+</div>
+
+</div>
+</nav>
+<!-- End Navigation -->
+</header>
+<!-- end #header -->
